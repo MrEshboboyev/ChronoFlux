83cbff8fde04ce443b2292fb5ab1f713fc9fcd1a
MrEshboboyev <mreshboboyev@gmail.com>
2025-03-19 16:52:20 +0500

    [Core/Marten] Enhance Marten integration with additional repository features and documentation
    - **MartenRepository Enhancements:**
      • Added support for batch operations and improved error handling within the Marten repository.
      • Introduced new methods for bulk updates and retrievals of aggregates, enhancing performance and usability.
    
    - **Documentation Improvements:**
      • Updated XML documentation and inline comments to clarify usage and improve maintainability for developers.
      • Enhanced test coverage for new features to ensure robustness and reliability.
    
    These changes aim to streamline interactions with the Marten repository and provide clearer guidance for developers integrating Marten into their applications.

---
+++ Core/Core.Marten/MartenConfig.cs
@@ -0,0 +1,131 @@
+using Core.Configuration;
+using Core.Ids;
+using Core.Marten.Commands;
+using Core.Marten.Ids;
+using Core.Marten.Subscriptions;
+using Marten;
+using Marten.Events.Daemon.Resiliency;
+using Marten.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+using Weasel.Core;
+
+namespace Core.Marten;
+
+public class MartenConfig
+{
+    private const string DefaultSchema = "public";
+
+    public string ConnectionString { get; set; } = default!;
+
+    public string WriteModelSchema { get; set; } = DefaultSchema;
+    public string ReadModelSchema { get; set; } = DefaultSchema;
+
+    public bool ShouldRecreateDatabase { get; set; } = false;
+
+    public DaemonMode DaemonMode { get; set; } = DaemonMode.Solo;
+
+    public bool UseMetadata = true;
+}
+
+public static class MartenConfigExtensions
+{
+    private const string DefaultConfigKey = "EventStore";
+
+    public static MartenServiceCollectionExtensions.MartenConfigurationExpression AddMarten(
+        this IServiceCollection services,
+        IConfiguration configuration,
+        Action<StoreOptions>? configureOptions = null,
+        string configKey = DefaultConfigKey,
+        bool useExternalBus = false,
+        bool disableAsyncDaemon = false
+    ) =>
+        services.AddMarten(
+            configuration.GetRequiredConfig<MartenConfig>(configKey),
+            configureOptions,
+            useExternalBus,
+            disableAsyncDaemon
+        );
+
+    public static MartenServiceCollectionExtensions.MartenConfigurationExpression AddMarten(
+        this IServiceCollection services,
+        MartenConfig martenConfig,
+        Action<StoreOptions>? configureOptions = null,
+        bool useExternalBus = false,
+        bool disableAsyncDaemon = false
+    )
+    {
+        var config = services
+            .AddScoped<IIdGenerator, MartenIdGenerator>()
+            .AddMarten(sp =>
+            {
+                var dataSource = sp.GetService<NpgsqlDataSource>();
+                if (dataSource != null)
+                {
+                    martenConfig.ConnectionString = dataSource.ConnectionString;
+                    Console.WriteLine(dataSource.ConnectionString);
+                }
+
+                return SetStoreOptions(martenConfig, configureOptions);
+            })
+            .UseLightweightSessions()
+            .ApplyAllDatabaseChangesOnStartup();
+
+        if (!disableAsyncDaemon)
+        {
+            //.OptimizeArtifactWorkflow()
+            config.AddAsyncDaemon(martenConfig.DaemonMode)
+                .AddSubscriptionWithServices<MartenEventPublisher>(ServiceLifetime.Scoped);
+        }
+
+        if (useExternalBus)
+            services.AddMartenAsyncCommandBus();
+
+
+        return config;
+    }
+
+    private static StoreOptions SetStoreOptions(
+        MartenConfig martenConfig,
+        Action<StoreOptions>? configureOptions = null
+    )
+    {
+        var options = new StoreOptions();
+        options.Connection(martenConfig.ConnectionString);
+        options.AutoCreateSchemaObjects = AutoCreate.CreateOrUpdate;
+
+        var schemaName = Environment.GetEnvironmentVariable("SchemaName");
+        options.Events.DatabaseSchemaName = schemaName ?? martenConfig.WriteModelSchema;
+        options.DatabaseSchemaName = schemaName ?? martenConfig.ReadModelSchema;
+
+        options.UseNewtonsoftForSerialization(
+            EnumStorage.AsString,
+            nonPublicMembersStorage: NonPublicMembersStorage.All
+        );
+
+        options.Projections.Errors.SkipApplyErrors = false;
+        options.Projections.Errors.SkipSerializationErrors = false;
+        options.Projections.Errors.SkipUnknownEvents = false;
+
+        if (martenConfig.UseMetadata)
+        {
+            options.Events.MetadataConfig.CausationIdEnabled = true;
+            options.Events.MetadataConfig.CorrelationIdEnabled = true;
+            options.Events.MetadataConfig.HeadersEnabled = true;
+        }
+
+        // Turn on Otel tracing for connection activity, and
+        // also tag events to each span for all the Marten "write"
+        // operations
+        options.OpenTelemetry.TrackConnections = TrackLevel.Normal;
+
+        // This opts into exporting a counter just on the number
+        // of events being appended. Kinda a duplication
+        options.OpenTelemetry.TrackEventCounters();
+
+        configureOptions?.Invoke(options);
+
+        return options;
+    }
+}
---
+++ Core/Core.Marten/OpenTelemetry/MartenRepositoryWithTelemetryDecorator.cs
@@ -0,0 +1,79 @@
+﻿using Core.Aggregates;
+using Core.Marten.OpenTelemetry;
+using Core.OpenTelemetry;
+using Marten;
+using Microsoft.Extensions.Logging;
+
+namespace Core.Marten.Repository;
+
+public class MartenRepositoryWithTracingDecorator<T>(
+    IMartenRepository<T> inner,
+    IDocumentSession documentSession,
+    IActivityScope activityScope,
+    ILogger<MartenRepositoryWithTracingDecorator<T>> logger)
+    : IMartenRepository<T>
+    where T : class, IAggregate
+{
+    public Task<T?> Find(Guid id, CancellationToken cancellationToken) =>
+        inner.Find(id, cancellationToken);
+
+    public Task<long> Add(Guid id, T aggregate, CancellationToken cancellationToken = default) =>
+        activityScope.RunAsync($"MartenRepository/{nameof(Add)}",
+            (activity, ct) =>
+            {
+                documentSession.PropagateTelemetry(activity, logger);
+
+                return inner.Add(id, aggregate, ct);
+            },
+            new StartActivityOptions
+            {
+                Tags =
+                {
+                    { TelemetryTags.Logic.EntityType, typeof(T).Name },
+                    { TelemetryTags.Logic.EntityId, id },
+                    { TelemetryTags.Logic.EntityVersion, aggregate.Version }
+                }
+            },
+            cancellationToken
+        );
+
+    public Task<long> Update(Guid id, T aggregate, long? expectedVersion = null, CancellationToken token = default) =>
+        activityScope.RunAsync($"MartenRepository/{nameof(Update)}",
+            (activity, ct) =>
+            {
+                documentSession.PropagateTelemetry(activity, logger);
+
+                return inner.Update(id, aggregate, expectedVersion, ct);
+            },
+            new StartActivityOptions
+            {
+                Tags =
+                {
+                    { TelemetryTags.Logic.EntityType, typeof(T).Name },
+                    { TelemetryTags.Logic.EntityId, id },
+                    { TelemetryTags.Logic.EntityVersion, aggregate.Version }
+                }
+            },
+            token
+        );
+
+    public Task<long> Delete(Guid id, T aggregate, long? expectedVersion = null, CancellationToken token = default) =>
+        activityScope.RunAsync($"MartenRepository/{nameof(Delete)}",
+            (activity, ct) =>
+            {
+                documentSession.PropagateTelemetry(activity, logger);
+
+                return inner.Delete(id, aggregate, expectedVersion, ct);
+            },
+            new StartActivityOptions
+            {
+                Tags =
+                {
+                    { TelemetryTags.Logic.EntityType, typeof(T).Name },
+                    { TelemetryTags.Logic.EntityId, id },
+                    { TelemetryTags.Logic.EntityVersion, aggregate.Version }
+                }
+            },
+            token
+        );
+}
