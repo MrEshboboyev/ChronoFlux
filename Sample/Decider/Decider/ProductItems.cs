da27040cc9f7d01e10fff44df5b2126045cd309f
MrEshboboyev <mreshboboyev@gmail.com>
2025-03-20 22:45:09 +0500

    Add Decider project and related components
    - Introduced the Decider project to the solution, including essential configurations and project files.
    - Added ProductItems and ShoppingCart classes to manage product and shopping cart functionalities.
    - Implemented event handling for shopping cart operations, including adding, removing, confirming, and canceling items.
    - Created Decider.Tests project with initial test configurations and references for testing the Decider functionality.
    - Updated Directory.Packages.props to include coverlet.collector for code coverage in tests.

---
+++ Sample/Decider/Decider/ProductItems.cs
@@ -0,0 +1,42 @@
+namespace Decider;
+
+public record ProductItem(
+    Guid ProductId,
+    int Quantity
+);
+
+public record PricedProductItem(
+    Guid ProductId,
+    int Quantity,
+    decimal UnitPrice
+);
+
+public class ProductItems
+{
+    public static ProductItems Empty = new([]);
+
+    public PricedProductItem[] Values { get; }
+
+    private ProductItems(PricedProductItem[] values) => Values = values;
+
+    public ProductItems Add(PricedProductItem productItem) =>
+        new(
+            [.. Values
+                .Concat([productItem])
+                .GroupBy(pi => pi.ProductId)
+                .Select(group => group.Count() == 1
+                    ? group.First()
+                    : group.First() with { Quantity = group.Sum(pi => pi.Quantity) }
+                )]
+        );
+
+    public ProductItems Remove(PricedProductItem productItem) =>
+        new(
+            [.. Values
+                .Select(pi => pi.ProductId == productItem.ProductId
+                    ? pi with { Quantity = pi.Quantity - productItem.Quantity }
+                    : pi
+                )
+                .Where(pi => pi.Quantity > 0)]
+        );
+}
