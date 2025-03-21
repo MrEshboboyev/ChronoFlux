5beb3f30699bb02c82da2dfb4c8b33e8355dbf31
MrEshboboyev <mreshboboyev@gmail.com>
2025-03-21 11:17:33 +0500

    Add EventPipelines project and MediatR integration
    - Introduced the EventPipelines project to the solution, including essential configurations and project files.
    - Added EventPipelines.MediatR and EventPipelines.Tests projects for handling event processing and testing functionalities.
    - Updated Directory.Packages.props to include MediatR package for improved command and query handling capabilities.
    - Enhanced solution structure with new project references and configurations to support event-driven architecture.

---
+++ Sample/EventPipelines/EventPipelines.MediatR/MediatorEventRouter.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace EventPipelines.MediatR;
+
+public class MediatorEventRouter<T>(IEventBus eventBus): INotificationHandler<T>
+    where T : INotification
+{
+    public async Task Handle(T notification, CancellationToken cancellationToken) =>
+        await eventBus.Publish(notification, cancellationToken);
+}
---
+++ Sample/EventPipelines/EventPipelines/Builder.cs
@@ -0,0 +1,80 @@
+﻿namespace EventPipelines;
+
+public class EventHandlersBuilder
+{
+    private readonly List<IEventHandler> eventHandlers = [];
+
+    private EventHandlersBuilder()
+    {
+
+    }
+
+    public static EventHandlersBuilder Setup() => new();
+
+    #region Handlers
+
+    public EventHandlersBuilder Handle<T>(T eventHandler)
+        where T : class, IEventHandler
+    {
+        eventHandlers.Add(eventHandler);
+        return this;
+    }
+
+    public EventHandlersBuilder Handle<TEvent>(
+        Action<TEvent> handler) =>
+        Handle(new EventHandlerWrapper<TEvent>(handler));
+
+    public EventHandlersBuilder Handle<TEvent>(
+        Func<TEvent, CancellationToken, ValueTask> handler) =>
+        Handle(new EventHandlerWrapper<TEvent>(handler));
+
+    public EventHandlersBuilder Handle<TEvent>(
+        IEventHandler<TEvent> handler) =>
+        Handle(handler as IEventHandler);
+
+    #endregion
+
+    #region Transforms
+
+    public EventHandlersBuilder Transform<TEvent, TTransformedEvent>(
+        Func<TEvent, TTransformedEvent> handler) =>
+        Handle(new EventTransformationWrapper<TEvent, TTransformedEvent>(handler));
+
+
+    public EventHandlersBuilder Transform<TEvent>(
+        Func<TEvent, object> handler) =>
+        Handle(new EventTransformationWrapper<TEvent>(handler));
+
+    public EventHandlersBuilder Transform<TEvent, TTransformedEvent>(
+        Func<TEvent, CancellationToken, ValueTask<TTransformedEvent>> handler) =>
+        Handle(new EventTransformationWrapper<TEvent, TTransformedEvent>(handler));
+
+
+    public EventHandlersBuilder Transform<TEvent>(
+        Func<TEvent, CancellationToken, ValueTask<object>> handler) =>
+        Handle(new EventTransformationWrapper<TEvent>(handler));
+
+    public EventHandlersBuilder Transform<TEvent, TTransformedEvent>(
+        IEventTransformation<TEvent, TTransformedEvent> handler) =>
+        Handle(handler);
+
+    #endregion
+
+    #region Filters
+
+    public EventHandlersBuilder Filter<TEvent>(
+        Func<TEvent, bool> handler) =>
+        Handle(new EventFilterWrapper<TEvent>(handler));
+
+    public EventHandlersBuilder Filter<TEvent>(
+        Func<TEvent, CancellationToken, ValueTask<bool>> handler) =>
+        Handle(new EventFilterWrapper<TEvent>(handler));
+
+    public EventHandlersBuilder Filter<TEvent>(
+        IEventFilter<TEvent> handler) =>
+        Handle(handler);
+
+    #endregion
+
+    public IEnumerable<IEventHandler> Build() => eventHandlers;
+}
