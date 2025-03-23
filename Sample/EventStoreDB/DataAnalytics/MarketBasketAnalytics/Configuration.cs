32151bac60c117989f973e27661eaf58d750f67c
MrEshboboyev <mreshboboyev@gmail.com>
2025-03-23 15:47:39 +0500

    Add Market Basket Analytics module with cart abandonment and product matching features
    - Introduced the Market Basket Analytics module, including core classes for handling cart abandonment rates and product item matching.
    - Implemented event handlers for shopping cart events, such as initialization, item addition, removal, confirmation, and abandonment.
    - Added configuration files for dependency injection and service registration.
    - Established data structures for calculating and summarizing cart abandonment rates, as well as managing product relationships in baskets.
    - Enhanced project structure with necessary references and configurations for integration with existing services.

---
+++ Sample/EventStoreDB/DataAnalytics/MarketBasketAnalytics/Configuration.cs
@@ -0,0 +1,14 @@
+﻿using MarketBasketAnalytics.CartAbandonmentRateAnalysis;
+using MarketBasketAnalytics.MarketBasketAnalysis;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MarketBasketAnalytics;
+
+public static class Configuration
+{
+    public static IServiceCollection AddMarketBasketAnalytics(this IServiceCollection services, IConfiguration configuration) =>
+        services
+            .AddCartAbandonmentRateAnalysis()
+            .AddMarketBasketAnalysis();
+}
---
+++ Sample/EventStoreDB/DataAnalytics/MarketBasketAnalytics/MarketBasketAnalysis/ProductRelationships.cs
@@ -0,0 +1,89 @@
+﻿namespace MarketBasketAnalytics.MarketBasketAnalysis;
+
+public record ProductRelationshipsInBaskets(
+    IReadOnlyList<Guid> RelatedProducts,
+    int BasketWithProductsCount
+);
+
+public record ProductRelationshipsCalculated(
+    Guid ProductId,
+    IReadOnlyList<ProductRelationshipsInBaskets> Relationships,
+    int BasketsCount
+)
+{
+    public static ProductRelationshipsCalculated Default() =>
+        new(default, [], default);
+}
+
+public static class ProductRelationships
+{
+    public static async Task<ProductRelationshipsCalculated> Handle(
+        Func<string, CancellationToken, Task<ProductRelationshipsCalculated?>> getCurrentSummary,
+        CartProductItemsMatched @event,
+        CancellationToken ct
+    )
+    {
+        var result = new List<ProductRelationshipsInBaskets>();
+
+        var currentSummary = await getCurrentSummary(ToStreamId(@event.ProductId), ct)
+            ?? ProductRelationshipsCalculated.Default();
+
+        var relatedProducts = Expand(@event.RelatedProducts).ToList();
+
+        foreach (var currentRel in currentSummary.Relationships)
+        {
+            var relationship = relatedProducts
+                .SingleOrDefault(rp => rp.SequenceEqual(currentRel.RelatedProducts));
+
+            if (relationship == null)
+            {
+                result.Add(currentRel);
+                continue;
+            }
+
+            result.Add(new ProductRelationshipsInBaskets(
+                relationship,
+                currentRel.BasketWithProductsCount + 1
+            ));
+            relatedProducts.Remove(relationship);
+        }
+
+        result.AddRange(
+            relatedProducts
+                .Select(relationship =>
+                    new ProductRelationshipsInBaskets(relationship, 1)
+                )
+        );
+
+        return currentSummary with { Relationships = result, BasketsCount = currentSummary.BasketsCount + 1 };
+    }
+
+    private static IReadOnlyList<IReadOnlyList<Guid>> Expand(IReadOnlyList<Guid> relatedProducts)
+        => [.. relatedProducts
+            .SelectMany(
+                (relatedProduct, index) =>
+                    Expand([relatedProduct], [.. relatedProducts.Skip(index + 1)])
+            )];
+
+    private static IEnumerable<IReadOnlyList<Guid>> Expand
+    (
+        IReadOnlyList<Guid> accumulator,
+        IReadOnlyList<Guid> relatedProducts
+    )
+    {
+        if (!relatedProducts.Any())
+            return [accumulator];
+
+        var aggregates = relatedProducts
+            .Select(relatedProduct => accumulator.Union([relatedProduct]).ToList())
+            .ToList();
+
+        return aggregates.Union(
+            aggregates.SelectMany((acc, i) => Expand(acc, [.. relatedProducts.Skip(i + 1)]))
+                .ToList()
+        );
+    }
+
+    public static string ToStreamId(Guid shoppingCartId) =>
+        $"cart_product_items_matching-{shoppingCartId}";
+}
