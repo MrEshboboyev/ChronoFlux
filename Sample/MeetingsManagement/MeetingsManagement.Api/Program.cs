327c484b9c741a7f4eb5c17d220013b37a07fc3e
MrEshboboyev <mreshboboyev@gmail.com>
2025-04-03 11:45:30 -0700

    Add MeetingsManagement.Api project and related updates
    - Included new project `MeetingsManagement.Api` in the solution.
    - Added build configurations for Debug and Release modes.
    - Updated `Directory.Packages.props` to reference `OpenTelemetry.Exporter.Jaeger`.
    - Added logging configurations in `appsettings.Development.json` and `appsettings.json`.
    - Updated `MeetingsManagement.Api.csproj` with necessary package and project references.
    - Enhanced `Program.cs` for web app setup, including Swagger and OpenTelemetry.
    - Introduced `MeetingsController` for handling meeting-related HTTP requests.
    - Created `launchSettings.json` for application launch profiles.

---
+++ Sample/MeetingsManagement/MeetingsManagement.Api/Program.cs
@@ -0,0 +1,58 @@
+using Core;
+using Core.Exceptions;
+using Core.Kafka;
+using Core.OpenTelemetry;
+using Core.WebApi.Middlewares.ExceptionHandling;
+using Core.WebApi.OptimisticConcurrency;
+using Core.WebApi.Swagger;
+using Marten.Exceptions;
+using MeetingsManagement;
+using Microsoft.OpenApi.Models;
+using OpenTelemetry.Trace;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Meeting Management", Version = "v1" });
+        c.OperationFilter<MetadataOperationFilter>();
+    })
+    .AddKafkaProducerAndConsumer()
+    .AddCoreServices()
+    .AddMeetingsManagement(builder.Configuration)
+    .AddDefaultExceptionHandler(
+        (exception, _) => exception switch
+        {
+            AggregateNotFoundException => exception.MapToProblemDetails(StatusCodes.Status404NotFound),
+            ConcurrencyException => exception.MapToProblemDetails(StatusCodes.Status412PreconditionFailed),
+            _ => null
+        })
+    .AddOptimisticConcurrencyMiddleware()
+    .AddOpenTelemetry("MeetingsManagement", OpenTelemetryOptions.Build(options =>
+        options.WithTracing(t =>
+            t.AddJaegerExporter()
+        ).DisableConsoleExporter(true)
+    ))
+    .AddControllers();
+
+var app = builder.Build();
+
+app.UseExceptionHandler()
+    .UseOptimisticConcurrencyMiddleware()
+    .UseRouting()
+    .UseAuthorization()
+    .UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllers();
+    })
+    .UseSwagger()
+    .UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Meeting Management V1");
+        c.RoutePrefix = string.Empty;
+    });
+
+app.Run();
+
+public partial class Program;
