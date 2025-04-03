c91b85c03c369401b8266d44808d9488d6ca149f
MrEshboboyev <mreshboboyev@gmail.com>
2025-04-03 11:48:43 -0700

    Add MeetingsSearch project and related functionality
    - Updated solution file to include the new "MeetingsSearch" project.
    - Modified AssemblyInfo.cs to expose internal members to integration tests.
    - Created Config.cs for Elasticsearch service configuration in MeetingsSearch.
    - Updated MeetingsSearch.csproj to reference Core.ElasticSearch and Core.
    - Added Config.cs in MeetingsSearch.Meetings for service registration.
    - Introduced Meeting class representing a meeting entity.
    - Added MeetingCreated class for event handling with Elasticsearch.
    - Created SearchMeetings class and handler for querying meetings.

---
+++ Sample/MeetingsManagement/MeetingsSearch/Meetings/CreatingMeeting/MeetingCreated.cs
@@ -0,0 +1,20 @@
+using Core.ElasticSearch.Repository;
+using Core.Events;
+
+namespace MeetingsSearch.Meetings.CreatingMeeting;
+
+internal class MeetingCreated(Guid meetingId, string name)
+{
+    public Guid MeetingId { get; } = meetingId;
+    public string Name { get; } = name;
+}
+
+internal class HandleMeetingCreated(IElasticSearchRepository<Meeting> repository): IEventHandler<MeetingCreated>
+{
+    public Task HandleAsync(MeetingCreated @event, CancellationToken cancellationToken)
+    {
+        var meeting = new Meeting(@event.MeetingId, @event.Name);
+
+        return repository.Add(@event.MeetingId, meeting, cancellationToken);
+    }
+}
---
+++ Sample/MeetingsManagement/MeetingsSearch/Meetings/SearchingMeetings/SearchMeetings.cs
@@ -0,0 +1,27 @@
+using Core.ElasticSearch.Indices;
+using Core.Queries;
+using Elastic.Clients.Elasticsearch;
+
+namespace MeetingsSearch.Meetings.SearchingMeetings;
+
+public class SearchMeetings(string filter)
+{
+    public string Filter { get; } = filter;
+}
+
+internal class HandleSearchMeetings(ElasticsearchClient elasticClient)
+    : IQueryHandler<SearchMeetings, IReadOnlyCollection<Meeting>>
+{
+    private const int MaxItemsCount = 1000;
+
+    private readonly ElasticsearchClient elasticClient = elasticClient ?? throw new ArgumentNullException(nameof(elasticClient));
+
+    public async Task<IReadOnlyCollection<Meeting>> HandleAsync(SearchMeetings query, CancellationToken cancellationToken)
+    {
+        var response = await elasticClient.SearchAsync<Meeting>(
+            s => s.Index(IndexNameMapper.ToIndexName<Meeting>())
+                .Query(q => q.QueryString(d => d.Query(query.Filter))).Size(MaxItemsCount), cancellationToken);
+
+        return response.Documents;
+    }
+}
