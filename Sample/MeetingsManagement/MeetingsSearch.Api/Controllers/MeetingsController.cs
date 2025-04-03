c1529744b6584bc231b4a1d891199412aae43ccb
MrEshboboyev <mreshboboyev@gmail.com>
2025-04-03 11:52:12 -0700

    Add MeetingsSearch.Api project and configuration files
    - Updated solution file to include MeetingsSearch.Api project.
    - Created appsettings.Development.json for logging settings.
    - Enhanced appsettings.json with logging, Elasticsearch, and Kafka configurations.
    - Modified MeetingsSearch.Api.csproj to use Microsoft.NET.Sdk.Web and added necessary package references.
    - Updated Program.cs to configure middleware, routing, and OpenTelemetry tracing.
    - Added MeetingsController for handling meeting search queries.
    - Created launchSettings.json for application launch profiles.

---
+++ Sample/MeetingsManagement/MeetingsSearch.Api/Controllers/MeetingsController.cs
@@ -0,0 +1,14 @@
+using Core.Queries;
+using MeetingsSearch.Meetings;
+using MeetingsSearch.Meetings.SearchingMeetings;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingsSearch.Api.Controllers;
+
+[Route("api/[controller]")]
+public class MeetingsController(IQueryBus queryBus): Controller
+{
+    [HttpGet]
+    public Task<IReadOnlyCollection<Meeting>> Search([FromQuery]string filter) =>
+        queryBus.QueryAsync<SearchMeetings, IReadOnlyCollection<Meeting>>(new SearchMeetings(filter));
+}
---
+++ Sample/MeetingsManagement/MeetingsSearch.Api/Program.cs
@@ -0,0 +1,47 @@
+using Core;
+using Core.Kafka;
+using Core.OpenTelemetry;
+using Core.WebApi.Middlewares.ExceptionHandling;
+using Core.WebApi.Swagger;
+using MeetingsSearch;
+using Microsoft.OpenApi.Models;
+using OpenTelemetry.Trace;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Meeting Search", Version = "v1" });
+        c.OperationFilter<MetadataOperationFilter>();
+    })
+    .AddKafkaConsumer()
+    .AddCoreServices()
+    .AddDefaultExceptionHandler()
+    .AddMeetingsSearch(builder.Configuration)
+    .AddOpenTelemetry("MeetingsSearch", OpenTelemetryOptions.Build(options =>
+        options.WithTracing(t =>
+            t.AddJaegerExporter()
+        ).DisableConsoleExporter(true)
+    ))
+    .AddControllers();
+
+var app = builder.Build();
+
+app.UseExceptionHandler()
+    .UseRouting()
+    .UseAuthorization()
+    .UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllers();
+    })
+    .UseSwagger()
+    .UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Meeting Search V1");
+        c.RoutePrefix = string.Empty;
+    });
+
+app.Run();
+
+public partial class Program;
