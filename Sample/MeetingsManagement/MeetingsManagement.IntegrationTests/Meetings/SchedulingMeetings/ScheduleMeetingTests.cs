4d6f92c5cf16b046e0bfabab2e20559a454a77cb
MrEshboboyev <mreshboboyev@gmail.com>
2025-04-03 21:45:28 -0700

    Add integration tests for MeetingsManagement and MeetingsSearch
    - Updated solution file to include new integration test projects.
    - Created `MeetingsManagement.IntegrationTests` and `MeetingsSearch.IntegrationTests` projects with necessary configurations and dependencies.
    - Added acceptance tests for creating and scheduling meetings using `Ogooreck.API`.
    - Defined API endpoints in `MeetingsManagementApi` and `MeetingsSearchApi`.
    - Updated `Settings.cs` to include Xunit configuration for both test projects.
    - Added `launchSettings.json` for environment configuration in both integration test projects.

---
+++ Sample/MeetingsManagement/MeetingsManagement.IntegrationTests/Meetings/SchedulingMeetings/ScheduleMeetingTests.cs
@@ -0,0 +1,48 @@
+using FluentAssertions;
+using MeetingsManagement.Meetings.CreatingMeeting;
+using MeetingsManagement.Meetings.GettingMeeting;
+using MeetingsManagement.Meetings.ValueObjects;
+using Xunit;
+using Ogooreck.API;
+using static Ogooreck.API.ApiSpecification;
+
+namespace MeetingsManagement.IntegrationTests.Meetings.SchedulingMeetings;
+
+public class ScheduleMeetingTests(ApiSpecification<Program> api): IClassFixture<ApiSpecification<Program>>
+{
+    [Fact]
+    [Trait("Category", "Acceptance")]
+    public async Task UpdateCommand_Should_Succeed()
+    {
+        await api
+            .Given(
+                "Created Meeting",
+                SEND(POST, URI(MeetingsManagementApi.MeetingsUrl), 
+                BODY(new CreateMeeting(MeetingId, MeetingName)))
+            )
+            .When(
+                POST,
+                URI(ctx => $"{MeetingsManagementApi.MeetingsUrl}/{ctx.GetCreatedId()}/schedule"),
+                BODY(DateRange.Create(Start, End)),
+                HEADERS(IF_MATCH(1))
+            )
+            .Then(OK)
+            .AndWhen(GET, URI(ctx => $"{MeetingsManagementApi.MeetingsUrl}/{ctx.GetCreatedId()}"))
+            .Until(RESPONSE_ETAG_IS(2))
+            .Then(
+                OK,
+                RESPONSE_BODY<MeetingView>(meeting =>
+                {
+                    meeting.Id.Should().Be(MeetingId);
+                    meeting.Name.Should().Be(MeetingName);
+                    meeting.Start.Should().Be(Start);
+                    meeting.End.Should().Be(End);
+                }));
+    }
+
+    private readonly DateTime Start = DateTime.UtcNow;
+    private readonly DateTime End = DateTime.UtcNow;
+
+    public readonly Guid MeetingId = Guid.NewGuid();
+    public readonly string MeetingName = "Event Sourcing Workshop";
+}
---
+++ Sample/MeetingsManagement/MeetingsSearch.IntegrationTests/Meetings/CreatingMeeting/CreateMeetingTests.cs
@@ -0,0 +1,51 @@
+using Core.Events;
+using Core.Testing;
+using FluentAssertions;
+using MeetingsSearch.Meetings;
+using MeetingsSearch.Meetings.CreatingMeeting;
+using Xunit;
+using Ogooreck.API;
+using static Ogooreck.API.ApiSpecification;
+
+namespace MeetingsSearch.IntegrationTests.Meetings.CreatingMeeting;
+
+public class CreateMeetingTests(TestWebApplicationFactory<Program> fixture)
+    : IClassFixture<TestWebApplicationFactory<Program>>, IDisposable
+{
+    private readonly ApiSpecification<Program> API = ApiSpecification<Program>.Setup(fixture);
+
+    [Fact]
+    [Trait("Category", "Acceptance")]
+    public async Task CreateCommand_ShouldPublish_MeetingCreateEvent()
+    {
+        await fixture.PublishInternalEvent(new EventEnvelope<MeetingCreated>(
+            new MeetingCreated(
+                MeetingId,
+                MeetingName
+            ),
+            new EventMetadata("event-id", 1, 2, null)
+        ));
+
+        await API.Given()
+            .When(
+                GET,
+                URI($"{MeetingsSearchApi.MeetingsUrl}?filter={MeetingName}")
+            )
+            .Until(
+                RESPONSE_BODY_MATCHES<IReadOnlyCollection<Meeting>>(
+                    meetings => meetings.Any(m => m.Id == MeetingId))
+            )
+            .Then(
+                RESPONSE_BODY<IReadOnlyCollection<Meeting>>(meetings =>
+                    meetings.Should().Contain(meeting =>
+                        meeting.Id == MeetingId
+                        && meeting.Name == MeetingName
+                    )
+                ));
+    }
+
+    private readonly Guid MeetingId = Guid.NewGuid();
+    private readonly string MeetingName = "Event Sourcing Workshop";
+
+    public void Dispose() => API.Dispose();
+}
