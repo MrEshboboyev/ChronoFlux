85a9e951362bd5f3a565ce6f4b0bf7cad394efbe
MrEshboboyev <mreshboboyev@gmail.com>
2025-04-03 22:17:25 -0700

    Add Warehouse.MinimalAPI project and related features
    - Updated solution file to include `Warehouse.MinimalAPI`.
    - Added logging configuration in `appsettings` files.
    - Modified `Program.cs` for web app setup with middleware and Swagger.
    - Updated `Warehouse.Api.csproj` with necessary package references.
    - Introduced `ICommandHandler<T>` and `IQueryHandler<T, TResult>` interfaces.
    - Added `EntitiesExtensions.cs` for entity management methods.
    - Created `MappingExtensions.cs` for value assertions.
    - Implemented product command and query handlers in new classes.
    - Defined `Product` class with SKU validation in `Product.cs`.
    - Added `launchSettings.json` for API project configuration.
    - Created `WarehouseDBContext.cs` for database context management.

---
+++ Sample/Warehouse.MinimalAPI/Warehouse.Api/Products/GetProductDetails.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Warehouse.Api.Core.Primitives;
+using Warehouse.Api.Core.Queries;
+
+namespace Warehouse.Api.Products;
+
+internal class HandleGetProductDetails(IQueryable<Product> products): IQueryHandler<GetProductDetails, ProductDetails?>
+{
+    public async ValueTask<ProductDetails?> Handle(GetProductDetails query, CancellationToken ct)
+    {
+        var product = await products
+            .SingleOrDefaultAsync(p => p.Id == query.ProductId, ct);
+
+        if (product == null)
+            return null;
+
+        return new ProductDetails(
+            product.Id,
+            product.Sku.Value,
+            product.Name,
+            product.Description
+        );
+    }
+}
+
+public record GetProductDetails(
+    Guid ProductId
+)
+{
+    public static GetProductDetails With(Guid? productId)
+        => new(productId.AssertNotEmpty(nameof(productId)));
+}
+
+public record ProductDetails(
+    Guid Id,
+    string Sku,
+    string Name,
+    string? Description
+);
