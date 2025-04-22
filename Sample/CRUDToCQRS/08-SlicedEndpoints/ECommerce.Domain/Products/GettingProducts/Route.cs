80cbe72adfdd46a49ac48affef4de0a9aca23420
MrEshboboyev <mreshboboyev@gmail.com>
2025-04-22 23:18:12 +0500

    Add ECommerce features and new project structure
    Updated solution to include "08-SlicedEndpoints" project with associated ECommerce.Domain and ECommerce projects. Modified ECommerce.Domain.csproj to use .NET SDK and added necessary package references. Introduced new configuration and extension classes for service registration and database operations. Added product entity definition and pagination functionality. Implemented endpoint routes for product management, including creation, deletion, and retrieval. Updated application settings and configured Swagger for API documentation. Adjusted launch settings for development environment.

---
+++ Sample/CRUDToCQRS/08-SlicedEndpoints/ECommerce.Domain/Products/GettingProducts/Route.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace ECommerce.Domain.Products.GettingProducts;
+
+public static class Route
+{
+    internal static IEndpointRouteBuilder UseGetProductsEndpoint(this IEndpointRouteBuilder endpoints)
+    {
+
+        endpoints.MapGet("/api/products/",
+            (
+                [FromServices] IQueryable<Product> products,
+                CancellationToken ct,
+                [FromQuery] int? pageNumber,
+                [FromQuery] int? pageSize
+            ) =>
+                products.HandleAsync(new GetProducts(pageNumber ?? 1, pageSize ?? 20), ct)
+        );
+        return endpoints;
+    }
+}
