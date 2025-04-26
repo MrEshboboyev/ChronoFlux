03b3e548bb747bef0e4114a337be3ffd9b500a15
MrEshboboyev <mreshboboyev@gmail.com>
2025-04-26 22:50:10 +0500

    Add projections and repository support for event sourcing
    Updated solution to include new projects for projections and Marten integration. Introduced `Aggregate`, `EventStore`, and repository classes to manage aggregates and events. Defined new interfaces for aggregate management and projections. Added test classes to validate functionality. Improved project structure to support CQRS patterns.

---
+++ Workshops/BuildYourOwnEventStore/09-Projections/Repository.cs
@@ -0,0 +1,19 @@
+namespace EventStoreBasics;
+
+public class Repository<T>(IEventStore eventStore): IRepository<T>
+    where T : IAggregate
+{
+    private readonly IEventStore eventStore = eventStore ?? throw  new ArgumentNullException(nameof(eventStore));
+
+    public T Find(Guid id) =>
+        eventStore.AggregateStream<T>(id);
+
+    public void Add(T aggregate) =>
+        eventStore.Store(aggregate);
+
+    public void Update(T aggregate) =>
+        eventStore.Store(aggregate);
+
+    public void Delete(T aggregate) =>
+        eventStore.Store(aggregate);
+}
