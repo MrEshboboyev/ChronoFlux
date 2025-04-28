8dd8f79d350016c0655518032b4268b6023b7902
MrEshboboyev <mreshboboyev@gmail.com>
2025-04-28 22:31:45 +0500

    Add event sourcing projects for Marten and EventStoreDB
    Updated solution to include two new projects:
    - `05-GettingStateFromEvents.Marten`
    - `06-GettingStateFromEvents.EventStoreDB`
    
    Created project files with necessary package references and SDK format.
    Introduced `GettingStateFromEventsTests` for both Marten and Event Store DB, validating shopping cart event handling.
    Added base test classes `MartenTest` and `EventStoreDBTest` for managing event appending and lifecycle.
    Overall, these changes establish a new structure for event sourcing and state management in the shopping cart system.

---
+++ Workshops/IntroductionToEventSourcing/06-GettingStateFromEvents.EventStoreDB/Tools/EventStoreDBTest.cs
@@ -0,0 +1,25 @@
+using System.Text.Json;
+using EventStore.Client;
+
+namespace IntroductionToEventSourcing.GettingStateFromEvents.Tools;
+
+public abstract class EventStoreDBTest: IDisposable
+{
+    protected readonly EventStoreClient EventStore = new(EventStoreClientSettings.Create("esdb://localhost:2113?tls=false"));
+
+    protected Task AppendEvents(string streamName, IEnumerable<object> events, CancellationToken ct) =>
+        // TODO: Fill append events logic here.
+        EventStore.AppendToStreamAsync(
+            streamName,
+            StreamState.Any,
+            events.Select(@event =>
+                new EventData(
+                    Uuid.NewUuid(),
+                    @event.GetType().FullName!,
+                    JsonSerializer.SerializeToUtf8Bytes(@event)
+                )
+            ), cancellationToken: ct);
+
+    public virtual void Dispose() =>
+        EventStore.Dispose();
+}
