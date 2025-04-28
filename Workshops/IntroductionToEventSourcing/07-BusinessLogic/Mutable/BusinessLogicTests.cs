99df00ffa0260fb99541715c37fa3847a8c6460c
MrEshboboyev <mreshboboyev@gmail.com>
2025-04-28 22:35:42 +0500

    Add Business Logic project and related functionality
    - Updated solution file to include "07-BusinessLogic" project.
    - Created project file with dependencies for testing.
    - Added unit tests for business logic using FluentAssertions and xunit.
    - Defined IProductPriceCalculator interface and a fake implementation for testing.
    - Updated ShoppingCart class to handle events for cart operations.
    - Implemented commands in ShoppingCartService for managing cart actions.
    - Added EventStore class for event sourcing capabilities.
    - Included a new image file related to the project.

---
+++ Workshops/IntroductionToEventSourcing/07-BusinessLogic/Mutable/BusinessLogicTests.cs
@@ -0,0 +1,99 @@
+using FluentAssertions;
+using IntroductionToEventSourcing.BusinessLogic.Tools;
+using Xunit;
+
+namespace IntroductionToEventSourcing.BusinessLogic.Mutable;
+
+using static ShoppingCartEvent;
+
+public static class ShoppingCartExtensions
+{
+    public static ShoppingCart GetShoppingCart(this EventStore eventStore, Guid shoppingCartId) =>
+        eventStore.ReadStream<ShoppingCartEvent>(shoppingCartId)
+            .Aggregate(ShoppingCart.Initial(), (shoppingCart, @event) =>
+            {
+                shoppingCart.Evolve(@event);
+                return shoppingCart;
+            });
+}
+
+public class BusinessLogicTests
+{
+    [Fact]
+    [Trait("Category", "SkipCI")]
+    public void RunningSequenceOfBusinessLogic_ShouldGenerateSequenceOfEvents()
+    {
+        var shoppingCartId = Guid.NewGuid();
+        var clientId = Guid.NewGuid();
+        var shoesId = Guid.NewGuid();
+        var tShirtId = Guid.NewGuid();
+        var twoPairsOfShoes = new ProductItem { ProductId = shoesId, Quantity = 2 };
+        var pairOfShoes = new ProductItem { ProductId = shoesId, Quantity = 1 };
+        var tShirt = new ProductItem { ProductId = tShirtId, Quantity = 1 };
+
+        var shoesPrice = 100;
+        var tShirtPrice = 50;
+
+        var pricedPairOfShoes = new PricedProductItem { ProductId = shoesId, Quantity = 1, UnitPrice = shoesPrice };
+        var pricedTShirt = new PricedProductItem { ProductId = tShirtId, Quantity = 1, UnitPrice = tShirtPrice };
+
+        var eventStore = new EventStore();
+
+        // Open
+        var shoppingCart = ShoppingCart.Open(shoppingCartId, clientId);
+        eventStore.AppendToStream(shoppingCartId, shoppingCart.UncommittedEvents);
+
+        // Add Two Pair of Shoes
+        shoppingCart = eventStore.GetShoppingCart(shoppingCartId);
+        shoppingCart.AddProduct(
+            FakeProductPriceCalculator.Returning(shoesPrice),
+            twoPairsOfShoes
+        );
+        eventStore.AppendToStream(shoppingCartId, shoppingCart.UncommittedEvents);
+
+        // Add T-Shirt
+        shoppingCart = eventStore.GetShoppingCart(shoppingCartId);
+        shoppingCart.AddProduct(
+            FakeProductPriceCalculator.Returning(tShirtPrice),
+            tShirt
+        );
+        eventStore.AppendToStream(shoppingCartId, shoppingCart.UncommittedEvents);
+
+        // Remove a pair of shoes
+        shoppingCart = eventStore.GetShoppingCart(shoppingCartId);
+        shoppingCart.RemoveProduct(pricedPairOfShoes);
+        eventStore.AppendToStream(shoppingCartId, shoppingCart.UncommittedEvents);
+
+        // Confirm
+        shoppingCart = eventStore.GetShoppingCart(shoppingCartId);
+        shoppingCart.Confirm();
+        eventStore.AppendToStream(shoppingCartId, shoppingCart.UncommittedEvents);
+
+        // Try Cancel
+        var exception = Record.Exception(() =>
+        {
+            shoppingCart = eventStore.GetShoppingCart(shoppingCartId);
+            shoppingCart.Cancel();
+            eventStore.AppendToStream(shoppingCartId, shoppingCart.UncommittedEvents);
+        });
+        exception.Should().BeOfType<InvalidOperationException>();
+
+        shoppingCart = eventStore.GetShoppingCart(shoppingCartId);
+
+        shoppingCart.Id.Should().Be(shoppingCartId);
+        shoppingCart.ClientId.Should().Be(clientId);
+        shoppingCart.ProductItems.Should().HaveCount(2);
+        shoppingCart.Status.Should().Be(ShoppingCartStatus.Confirmed);
+
+        shoppingCart.ProductItems[0].Should().BeEquivalentTo(pricedPairOfShoes);
+        shoppingCart.ProductItems[1].Should().BeEquivalentTo(pricedTShirt);
+
+        var events = eventStore.ReadStream<ShoppingCartEvent>(shoppingCartId);
+        events.Should().HaveCount(5);
+        events[0].Should().BeOfType<ShoppingCartOpened>();
+        events[1].Should().BeOfType<ProductItemAddedToShoppingCart>();
+        events[2].Should().BeOfType<ProductItemAddedToShoppingCart>();
+        events[3].Should().BeOfType<ProductItemRemovedFromShoppingCart>();
+        events[4].Should().BeOfType<ShoppingCartConfirmed>();
+    }
+}
