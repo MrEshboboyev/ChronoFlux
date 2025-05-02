f1b13f9527b631dd8143c96e95eb7257d94176e5
MrEshboboyev <mreshboboyev@gmail.com>
2025-05-02 22:39:10 +0500

    Add tests for EventStoreDB shopping cart functionality
    - Introduced a new project "11-OptimisticConcurrency.EventStoreDB.Tests" in the solution.
    - Updated solution configuration to include build settings for the new test project.
    - Created project file with necessary properties and package references.
    - Added test classes for shopping cart operations using Xunit.
    - Implemented utility methods in `Fixtures.cs` for API testing scenarios.
    - Covered various shopping cart functionalities in multiple test files.

---
+++ Workshops/IntroductionToEventSourcing/11-OptimisticConcurrency.EventStoreDB.Tests/ShoppingCarts/ConfirmShoppingCartTests.cs
@@ -0,0 +1,107 @@
+using Bogus;
+using Ogooreck.API;
+using OptimisticConcurrency.Immutable.ShoppingCarts;
+using Xunit;
+using static Ogooreck.API.ApiSpecification;
+using static OptimisticConcurrency.EventStoreDB.Tests.ShoppingCarts.Scenarios;
+using static OptimisticConcurrency.EventStoreDB.Tests.ShoppingCarts.Fixtures;
+
+namespace OptimisticConcurrency.EventStoreDB.Tests.ShoppingCarts;
+
+public class ConfirmShoppingCartTests(ApiSpecification<Program> api):
+    IClassFixture<ApiSpecification<Program>>
+{
+    [Theory]
+    [Trait("Category", "SkipCI")][InlineData("immutable")]
+    [InlineData("mutable")]
+    [InlineData("mixed")]
+    public Task CantConfirmNotExistingShoppingCart(string apiPrefix) =>
+        api.Given()
+            .When(
+                POST,
+                URI(ConfirmShoppingCartUrl(apiPrefix, ClientId, NotExistingShoppingCartId)),
+                HEADERS(IF_MATCH(-1))
+            )
+            .Then(NOT_FOUND);
+
+    [Theory]
+    [Trait("Category", "SkipCI")][InlineData("immutable")]
+    [InlineData("mutable")]
+    [InlineData("mixed")]
+    public Task CantConfirmEmptyShoppingCart(string apiPrefix) =>
+        api.Given(OpenedShoppingCart(apiPrefix, ClientId))
+            .When(
+                POST,
+                URI(ctx => ConfirmShoppingCartUrl(apiPrefix, ClientId, ctx.GetCreatedId<Guid>())),
+                HEADERS(IF_MATCH(0))
+            )
+            .Then(CONFLICT);
+
+    [Theory]
+    [Trait("Category", "SkipCI")][InlineData("immutable")]
+    [InlineData("mutable")]
+    [InlineData("mixed")]
+    public Task ConfirmsNonEmptyShoppingCart(string apiPrefix) =>
+        api.Given(
+                OpenedShoppingCart(apiPrefix, ClientId),
+                WithProductItem(apiPrefix, ClientId, ProductItem, 0)
+            )
+            .When(
+                POST,
+                URI(ctx => ConfirmShoppingCartUrl(apiPrefix, ClientId, ctx.GetCreatedId<Guid>())),
+                HEADERS(IF_MATCH(1))
+            )
+            .Then(NO_CONTENT, RESPONSE_ETAG_HEADER(2));
+
+    [Theory]
+    [Trait("Category", "SkipCI")][InlineData("immutable")]
+    [InlineData("mutable")]
+    [InlineData("mixed")]
+    public Task CantConfirmAlreadyConfirmedShoppingCart(string apiPrefix) =>
+        api.Given(
+                OpenedShoppingCart(apiPrefix, ClientId),
+                WithProductItem(apiPrefix, ClientId, ProductItem, 0),
+                ThenConfirmed(apiPrefix, ClientId, 1)
+            )
+            .When(
+                POST,
+                URI(ctx => ConfirmShoppingCartUrl(apiPrefix, ClientId, ctx.GetCreatedId<Guid>())),
+                HEADERS(IF_MATCH(2))
+            )
+            .Then(CONFLICT);
+
+    [Theory]
+    [Trait("Category", "SkipCI")][InlineData("immutable")]
+    [InlineData("mutable")]
+    [InlineData("mixed")]
+    public Task CantConfirmCanceledShoppingCart(string apiPrefix) =>
+        api.Given(
+                OpenedShoppingCart(apiPrefix, ClientId),
+                WithProductItem(apiPrefix, ClientId, ProductItem, 0),
+                ThenCanceled(apiPrefix, ClientId, 1)
+            )
+            .When(
+                POST,
+                URI(ctx => ConfirmShoppingCartUrl(apiPrefix, ClientId, ctx.GetCreatedId<Guid>())),
+                HEADERS(IF_MATCH(2))
+            )
+            .Then(CONFLICT);
+
+    [Theory]
+    [Trait("Category", "SkipCI")][InlineData("immutable")]
+    [InlineData("mutable")]
+    [InlineData("mixed")]
+    public Task ReturnsNonEmptyShoppingCart(string apiPrefix) =>
+        api.Given(
+                OpenedShoppingCart(apiPrefix, ClientId),
+                WithProductItem(apiPrefix, ClientId, ProductItem, 0),
+                ThenConfirmed(apiPrefix, ClientId, 1)
+            )
+            .When(GET, URI(ctx => ShoppingCartUrl(apiPrefix, ClientId, ctx.GetCreatedId<Guid>())))
+            .Then(OK, RESPONSE_ETAG_HEADER(2));
+
+    private static readonly Faker Faker = new();
+    private readonly Guid NotExistingShoppingCartId = Guid.NewGuid();
+    private readonly Guid ClientId = Guid.NewGuid();
+    private readonly ProductItemRequest ProductItem = new(Guid.NewGuid(), Faker.Random.Number(1, 500));
+}
---
+++ Workshops/IntroductionToEventSourcing/11-OptimisticConcurrency.EventStoreDB.Tests/ShoppingCarts/OpenShoppingCartTests.cs
@@ -0,0 +1,34 @@
+using Ogooreck.API;
+using Xunit;
+using static Ogooreck.API.ApiSpecification;
+using static OptimisticConcurrency.EventStoreDB.Tests.ShoppingCarts.Scenarios;
+using static OptimisticConcurrency.EventStoreDB.Tests.ShoppingCarts.Fixtures;
+
+namespace OptimisticConcurrency.EventStoreDB.Tests.ShoppingCarts;
+
+public class OpenShoppingCartTests(ApiSpecification<Program> api):
+    IClassFixture<ApiSpecification<Program>>
+{
+    [Theory]
+    [Trait("Category", "SkipCI")][InlineData("immutable")]
+    [InlineData("mutable")]
+    [InlineData("mixed")]
+    public Task OpensShoppingCart(string apiPrefix) =>
+        api.Given()
+            .When(POST, URI(ShoppingCartsUrl(apiPrefix, ClientId)))
+            .Then(
+                CREATED_WITH_DEFAULT_HEADERS(locationHeaderPrefix: ShoppingCartsUrl(apiPrefix, ClientId)),
+                RESPONSE_ETAG_HEADER(0)
+            );
+
+    [Theory]
+    [Trait("Category", "SkipCI")][InlineData("immutable")]
+    [InlineData("mutable")]
+    [InlineData("mixed")]
+    public Task ReturnsOpenedShoppingCart(string apiPrefix) =>
+        api.Given(OpenedShoppingCart(apiPrefix, ClientId))
+            .When(GET, URI(ctx => ShoppingCartUrl(apiPrefix, ClientId, ctx.GetCreatedId<Guid>())))
+            .Then(OK, RESPONSE_ETAG_HEADER(0));
+
+    private readonly Guid ClientId = Guid.NewGuid();
+}
